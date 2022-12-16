f02e99ccaead199b8d7606d721b8e614f3c945e3
juanmera01 <Uo277406@uniovi.es>
2022-12-16 16:02:53 +0100

    initial commit

---
+++ src/test/java/com/juan/starwarsapi/StarwarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.starwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
